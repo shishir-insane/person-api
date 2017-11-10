78eafb1bd02ee800ff8dceeee92904e01d96ae8a
Shishir Kumar <shishir.insane@gmail.com>
2017-11-10 22:14:43 +0000

    Base framework setup

---
+++ src/main/java/com/sk/wagawin/person/PersonApiApplication.java
@@ -0,0 +1,12 @@
+package com.sk.wagawin.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sk/wagawin/person/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sk.wagawin.person;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PersonApiApplication.class);
+	}
+
+}
